71beaefd3a071880da1aafe10823590f0236e047
MikhailPilipenko <pmgomel@mail.ru>
2021-04-08 22:06:27 +0300

    feat: add function find-index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,25 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let found = false;
+  let first = 0;
+  let last = array.length - 1;
+  let index;
+
+  while (found === false && first <= last) {
+    const mid = Math.floor((first + last) / 2);
+    if (array[mid] === value) {
+      found = true;
+      index = mid;
+    } else if (array[mid] > value) {
+      last = mid - 1;
+    } else {
+      first = mid + 1;
+    }
+  }
+
+  return index;
 }
 
 module.exports = findIndex;
